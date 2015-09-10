28955844592155abf506bff3000e736986cf3799
John Slota <john.slota@gmail.com>
2015-09-09 19:47:38 -0600

    day 2 of linked list

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,8 +7,6 @@ class LinkedList
     # @counter = 0
   end
 
-
-
   def tail_data
     tail.data
   end
@@ -38,29 +36,80 @@ class LinkedList
   end
 
   def insert(data, position)
-    check_where_to_insert
+    if position == 0
+      prepends(data)
+    else
+      previous = check_where_to_insert(data, position)
+      insert_new_node(data, previous)
+    end
+  end
 
-
+  def insert_new_node(data, previous)
+    new_node = Node.new(data)
+    new_node.next_node = previous.next_node
+    previous.next_node = new_node
   end
 
   def check_where_to_insert(data, position)
-    current = @head
+    previous = @head
     position_check = 0
-    until position_check == position
-      current = current.next_node
+    until position_check == (position-1)
+      previous  = previous.next_node
       position_check +=1
     end
-    current
+    previous
   end
 
+  def count(data)
+    @counter += 1
+  end
 
-  # def count(data)
-  #   @counter += 1
-  # end
-  #
-  # def count_total
-  #   @counter
-  # end
+  def count_total
+    @counter
+  end
 
+  def all
+    current = @head
+    array_string = ''
+    until current.next_node == nil
+      array_string << "#{current.data} "
+      current = current.next_node
+    end
+    array_string << "#{current.data}"
+  end
 
+  def all_nodes
+    current = @head
+  end
+
+  def includes?(data)
+    i = 0
+    array = all.split
+    until i > array.length || data == array[i]
+      i += 1
+    end
+    data == array[i]
+  end
+
+  def count
+    array = all.split
+    array.count
+  end
+
+  def pop
+    tail_delete
+    data = tail_delete.next_node.data
+    tail_delete.next_node = nil
+    data
+  end
+
+  def tail_delete
+    current  = @head
+    previous = @head
+    until current.next_node.nil?
+      previous = current
+      current = current.next_node
+    end
+    previous
+  end
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -7,6 +7,11 @@ class LinkedListTest < Minitest::Test
     assert_equal "test", node.head.data
   end
 
+  def test_heads_next_node_is_null
+    node = LinkedList.new("test_nil")
+    assert_equal nil, node.head.next_node
+  end
+
   def test_it_accepts_zero_data
     node = LinkedList.new(nil)
     assert_equal nil, node.head.data
@@ -22,7 +27,7 @@ class LinkedListTest < Minitest::Test
     assert_equal "tail", node.tail_data
   end
 
-  def test_it_gets_tail_data
+  def test_it_gets_head_data
     node = LinkedList.new("head")
     assert_equal "head", node.head_data
   end
@@ -44,6 +49,74 @@ class LinkedListTest < Minitest::Test
     node.append("jersey mike")
     node.insert("tangarang", 1)
     node.insert("hola", 1)
-    assert_equal "tangarang", node.tail_data
+    assert_equal "jersey mike", node.tail_data
   end
+
+  def test_it_lists_them_all
+    node = LinkedList.new("first node")
+    node.append("jersey mike second node")
+    node.append("tangarang third node")
+    assert_equal "first node jersey mike second node tangarang third node", node.all
+  end
+
+  def test_inserts_in_a_kewler_way
+    node = LinkedList.new("head")
+    node.append("jersey mike")
+    node.insert("tangarang", 1)
+    node.insert("hola", 1)
+    assert_equal "head hola tangarang jersey mike", node.all
+  end
+
+  def test_what_if_we_insert_at_zero
+    node = LinkedList.new("head_second")
+    node.append("jersey mike")
+    node.insert("hola", 0)
+    assert_equal "hola head_second jersey mike", node.all
+  end
+
+  def test_lets_insert_at_the_end
+    node = LinkedList.new("head")
+    node.append("jersey mike")
+    node.insert("tail", 2)
+    assert_equal "head jersey mike tail", node.all
+  end
+
+  def test_does_it_include?
+    node = LinkedList.new("head")
+    node.append("jersey mike")
+    node.insert("tail", 1)
+    assert_equal true, node.includes?("tail")
+  end
+
+  def test_its_a_liar?
+    node = LinkedList.new("head")
+    node.append("jersey mike")
+    node.insert("tail", 1)
+    assert_equal false, node.includes?("Raiders Rule")
+  end
+
+  def test_it_knows_the_count
+    node = LinkedList.new("head")
+    node.append("jersey")
+    node.insert("tail", 1)
+    assert_equal 3, node.count
+  end
+
+  def test_it_pops_a_lop
+    node = LinkedList.new("head")
+    node.append("jersey")
+    node.append("tyler")
+    node.append("john")
+    assert_equal "john", node.pop
+    assert_equal "tyler", node.tail_data
+  end
+
+  # def test_it_gets_all_the_nodes
+  #   node = LinkedList.new("first node")
+  #   node.append("jersey mike second node")
+  #   node.append("tangarang third node")
+  #   assert_equal "#<Node:0xXXXXXX @data='first node', @next_node=#<Node:0xXXXXXX @data='jersey mike second node', @next_node=#<Node:0xXXXXXX @data='tangarang third node', @next_node=nil>>>", node.all_nodes
+  # end
+
+
 end
